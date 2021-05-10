7ac3232b4b84fc02a5a8f0df20d48980ba047be9
caizhicheng <caizhicheng@tal.com>
2021-05-10 19:56:35 +0800

    fix: go-zero 基本架构代码

---
+++ api/internal/svc/servicecontext.go
@@ -0,0 +1,23 @@
+package svc
+
+import (
+	"resk/api/internal/config"
+	"resk/rpc/add/adder"
+	"resk/rpc/check/checker"
+
+	"github.com/tal-tech/go-zero/zrpc"
+)
+
+type ServiceContext struct {
+	Config  config.Config
+	Adder   adder.Adder
+	Checker checker.Checker
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config:  c,
+		Adder:   adder.NewAdder(zrpc.MustNewClient(c.Add)),
+		Checker: checker.NewChecker(zrpc.MustNewClient(c.Check)),
+	}
+}
